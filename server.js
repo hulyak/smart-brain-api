140e40562463a3c35aa3c6a4494b0eae36773013
hulyak <57311842+hulya95@users.noreply.github.com>
2020-08-09 12:13:17 -0700

    feat: connect server to knex

---
+++ server.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+
+const register = require('./controllers/register.js');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host: '127.0.0.1', //localhost
+    user: 'hulyakarakaya',
+    password: '',
+    database: 'smart-brain',
+  },
+});
+
+db.select('*').from('users').then((data) => {
+  console.log(data);
+});
+
+app.get('/', (req, res) => {
+  res.send(database.users);
+});
+
+app.post('/signin', (req, res) => {
+  signin.handleSignin(req, res, db, bcrypt);
+});
+
+app.post('/register', (req, res) => {
+  register.handleRegister(req, res, db, bcrypt); //inject db and bcrypt to register.js
+});
+
+app.get('/profile/:id', (req, res) => {
+  profile.handleProfileGet(req, res, db);
+});
+
+app.put('/image', (req, res) => {
+  image.handleImage(req, res, db);
+});
+
+//clarifai api connection
+app.post('/imageUrl', (req, res) => {
+  image.handleApiCall(req, res);
+});
+
+app.listen(3030, () => {
+  console.log('app is running on port 3030');
+});
+
+//POSTGRES SQL
+// hyper -> desktop -> createdb smart-brain
+// psql 'smart-brain'
+//open postbird and connect to smart-brain
+
+// CREATE TABLE users (
+//   id serial PRIMARY KEY,
+//   name varchar(100),
+//   email text UNIQUE NOT NULL,
+//   entries BIGINT DEFAULT 0,
+//   joined TIMESTAMP NOT NULL
+// );
+
+// \d command shows the table postbird
+
+// CREATE TABLE login (
+// 	id SERIAL PRIMARY KEY,
+//   hash varchar(100) NOT NULL,
+//   email text UNIQUE NOT NULL
+// );
+
+//CONNECT TO  DATABASE TO SERVER
+//https://github.com/vitaly-t/pg-promise
+//http://knexjs.org/      node
